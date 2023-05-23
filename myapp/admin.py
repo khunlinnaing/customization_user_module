24f407baf298ba9681390ba1d74bbc642123109d
khunlinnaingoo <khunlinnaing90@gmail.com>
2023-05-23 16:20:47 +0630

    customizations user models

---
+++ myapp/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from myapp.models import NewUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
+
+class UserAdminConfig(UserAdmin):
+    ordering = ('-start_date',)
+    list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name')}),
+        ("Permissions", {'fields': ('is_staff', 'is_active')}),
+        ("Personal", {'fields': ('about',)})
+    )
+
+    formfield_overrides = {
+        NewUser.about: {'widget': Textarea(attrs={'row': 10, 'cols': 40})}
+    }
+
+    add_fieldsets =(
+        (None,{
+            'classes': ('wide',),
+            'fields':('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active','is_staff')
+        }),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
+# admin.site.register(NewUser)
